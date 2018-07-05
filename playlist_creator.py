e42ff6852e05ddbcebac92e74bad40b683de483e
Matt Cole <ccmatt19@gmail.com>
2018-07-05 18:54:24 -0400

    Program successfully scrapes top 100 and creates normalized list of songs I want on my playlist

--- playlist_creator.py
+++ playlist_creator.py
@@ -4,12 +4,36 @@ import os
 import spotipy
 import spotipy.util as util
 
+
 '''Scrapes hotnewhiphop top 100 songs then creates and returns list of songs by artists I enjoy
 parameters:
     fav_artists - artists I want songs from '''
 def getTopSongs(fav_artists):
+    artists = []
+    song_titles = []
     songs = []
-    return
+    url = 'https://www.hotnewhiphop.com/top100/'
+    source = requests.get(url).text
+    soup = BeautifulSoup(source, 'html.parser')
+    for song in soup.find_all('div', class_='chartItem-body-artist'):
+        song_title = song.a.text.strip()
+        for artist in song.find('strong', class_='chartItem-artist-artistName'):
+            if artist in fav_artists:
+                songs.append([song_title, artist])
+    return songs
+
+'''Normalizes song name.  Removes producer name, and other aspects that will effect the song being found on Spotify
+parameters:
+    songs - list of songs to normalize'''
+def normalizeSongs(songs):
+    for song in songs:
+        song_title = song[0]
+        if '(' in song_title:
+            index = song_title.find('(')
+            song_title = song_title[0: index-1]
+        song[0] = song_title
+    print(songs)
+    return songs
 
 '''Searches Spotify for song.  If the song is found, the function returns the song id. If the song is not found it returns False
 parameters:
@@ -25,13 +49,15 @@ parameters:
 def addSong(id, playlist):
     return
 
+
 def main():
     desired_artists = ['Drake']
     desired_songs = getTopSongs(desired_artists)
+    songs = normalizeSongs(desired_songs)
     song_ids = []
     master_ids = [] #needs to be separate file to avoid being overwritten
     #create Spotify session
-    desired_playlist = '18July'#need function to create playlist / go to desired playlist each month
+    desired_playlist = '18July' #need function to create playlist / go to desired playlist each month
     for song in desired_songs:
         song_id = spotifySearch(song)
         if song_id:
@@ -40,8 +66,6 @@ def main():
         if song_id not in master_ids:
             addSong(song_id, desired_playlist)
     print('Program complete!')
-
-
 
 
 if __name__ == '__main__':
