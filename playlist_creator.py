8f5eb75cd78427e1dc74db362bcbc21f2dd905c4
Matt Cole <ccmatt19@gmail.com>
2018-07-09 20:03:33 -0400

    Finds correct playlist to add songs to

--- playlist_creator.py
+++ playlist_creator.py
@@ -4,7 +4,6 @@ import os
 import spotipy
 import spotipy.util as util
 import datetime
-import json
 
 
 '''Scrapes hotnewhiphop top 100 songs then creates and returns list of songs by artists I enjoy
@@ -24,7 +23,7 @@ def getTopSongs(fav_artists):
                 songs.append([song_title, artist])
     return songs
 
-'''Normalizes song name.  Removes producer name, and other aspects that will effect the song being found on Spotify
+'''Normalizes song name.  Removes producer name, and other aspects that will affect the song being found on Spotify
 parameters:
     songs - list of songs to normalize'''
 def normalizeSongs(songs):
@@ -34,7 +33,7 @@ def normalizeSongs(songs):
             index = song_title.find('(')
             song_title = song_title[0: index-1]
         song[0] = song_title
-    print('Songs that I want: ')
+    print("Songs that I want from HotNewHipHop's top 100: ")
     print('--------------------')
     for song in songs:
         print('{} - {}'.format(song[0], song[1]))
@@ -52,7 +51,6 @@ def getToken():
     access_token = util.prompt_for_user_token(username=user, scope=desired_scope, client_id=id, client_secret=secret,
                                        redirect_uri=uri)
     if access_token:
-        print('Token gathered successfully!\n')
         return access_token
     else:
         print('ERROR obtaining token.')
@@ -88,6 +86,17 @@ def createPlaylist(sp, playlist_name):
     print('New playlist, {}, created'.format(playlist_name))
     return
 
+'''Obtains and returns desired playlist's id.
+parameters:
+    sp - spotify session
+    playlist_name - name of playlist to get id for'''
+def getPlaylistID(sp, playlist_name):
+    playlists = sp.user_playlists('ccmatt19')
+    for playlist in playlists['items']:
+        if playlist['name'] == playlist_name:
+            id = playlist['id']
+    return id
+
 '''Searches Spotify for song.  If the song is found, the function returns the song id. If the song is not found it returns False
 parameters:
     sp - spotify session
@@ -100,6 +109,7 @@ def spotifySearch(sp, song):
     for result in search_query['tracks']['items']:
         tracks.append(result['external_urls'])
     if not tracks:
+        print('{} - {}'.format(song[0], song[1]))
         return
     track = tracks[0]
     wanted_track = track['spotify']
@@ -130,17 +140,17 @@ parameters:
     sp - spotify session
     id - song id for song to add
     playlist - monthly playlist name that song will be added to'''
-def addSong(sp, id, playlist):
-    playlist_id = '4p6VOeZCjgjH8zgxgDE5Xh'
+def addSong(sp, id, playListID):
     track_uri = [id]
     print(track_uri)
-    sp.user_playlist_add_tracks('ccmatt19', playlist_id, track_uri)
+    sp.user_playlist_add_tracks('ccmatt19', playListID, track_uri)
     return
 
 
 def main():
-    desired_artists = ['Drake', 'Nav', 'Machine Gun Kelly', 'A$AP Rocky', 'NF', 'Post Malone', 'Chance The Rapper', 'J. Cole', 'Juice WRLD'
-                       'Kanye West', 'Kid Cudi', 'Kendrick Lamar', 'Lil Uzi Vert', 'Russ', 'Meek Mill']
+    desired_artists = ['Drake', 'Nav', 'Machine Gun Kelly', 'A$AP Rocky', 'NF', 'Post Malone', 'Chance The Rapper', 'J. Cole', 'Juice WRLD',
+                       'Kanye West', 'Kid Cudi', 'Kendrick Lamar', 'Lil Uzi Vert', 'Russ', 'B.o.B', 'Lil Dicky', 'Chris Webby', 'Eminem',
+                       'Travis Scott', 'Flatbush Zombies', 'Logic', 'Trippie Redd', 'Vic Mensa', 'Young Thug', 'Mac Miller']
     desired_songs = getTopSongs(desired_artists)
     songs = normalizeSongs(desired_songs)
     song_ids = []
@@ -151,6 +161,9 @@ def main():
     if not playlistExists(session, desired_playlist):
         createPlaylist(session, desired_playlist)
         print()
+    playlistID = getPlaylistID(session, desired_playlist)
+    print('Songs not found on Spotify: ')
+    print('---------------------------')
     for song in desired_songs:
         song_id = spotifySearch(session, song)
         if song_id:
@@ -158,10 +171,10 @@ def main():
     master_file_contents = readFile()
     for song_id in song_ids:
         if song_id not in master_file_contents:
-            addSong(session, song_id, desired_playlist)
+            addSong(session, song_id, playlistID)
             writeToFile(song_id)
 
-    print('\nProgram complete!')
+    print('\nPROGRAM COMPLETE!')
 
 
 if __name__ == '__main__':
